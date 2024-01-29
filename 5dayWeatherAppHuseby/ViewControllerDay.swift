ef7c997b0ecef88e99728ab06ceb104cd3253bf1
Catherine <catherine.huseby24@d155.org>
2024-01-29 07:25:13 -0600

    pls work

--- 5dayWeatherAppHuseby/ViewControllerDay.swift
+++ 5dayWeatherAppHuseby/ViewControllerDay.swift
@@ -8,11 +8,76 @@
 import UIKit
 
 class ViewControllerDay: UIViewController {
-
+    
+  //  "Drizzle" : "🌦️",
+    //    "ThunderStorm" : "⛈️",
+    //    "Snow" : "❄️",
+     //   "Rain" : "🌧️",
+     //   "Clear" : "☀️",
+     //   "Clouds" : "☁️"
+    
+    
+  //  @IBOutlet weak var tempOutlet: UILabel!
+    
+    @IBOutlet weak var todayOutlet: UILabel!
+    
+    @IBOutlet weak var highOutlet: UILabel!
+    
+    @IBOutlet weak var lowOutlet: UILabel!
+    
+    @IBOutlet weak var feelsLikeOutlet: UILabel!
+    
+    @IBOutlet weak var humidityOutlet: UILabel!
+    
+    @IBOutlet weak var tempOutlet: UILabel!
+    
+    
+    @IBOutlet weak var tempDescOutlet: UILabel!
+    
+    
+    @IBOutlet weak var moreDescOutlet: UILabel!
+    
+    //var tempDesc = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        todayOutlet.text = "\(appData.dateList[appData.index]), \(appData.longDateList[appData.index])"
+        highOutlet.text = "High: \(appData.forecastList[appData.index].main.temp_max) degrees"
+        lowOutlet.text = "Low: \(appData.forecastList[appData.index].main.temp_min) degrees"
+        feelsLikeOutlet.text = "Feels like: \(appData.forecastList[appData.index].main.feels_like) degrees"
+        humidityOutlet.text = "Humidity: \(appData.forecastList[appData.index].main.humidity)%"
+        tempOutlet.text = "Temprature: \(appData.forecastList[appData.index].main.temp) degrees"
+        
+        switch appData.forecastList[appData.index].weather[0].main {
+        
+        case "Clouds":
+            tempDescOutlet.text = "☁️ Looking Cloudy!"
+            
+        case "Clear":
+            tempDescOutlet.text = "☀️ All Clear!"
+            
+        case "Rain":
+            tempDescOutlet.text = "🌧️ Rainy!"
+            
+        case "Snow":
+            tempDescOutlet.text = "❄️ Snowy!"
+            
+        case "ThunderStorm":
+            tempDescOutlet.text = "⛈️ Thunderstorm :("
+            
+        case "Drizzle":
+            tempDescOutlet.text = "🌦️ Drizzle!"
+            
+        default:
+            tempDescOutlet.text = "ZOMBIE APOCOLYPSE"
+        }
+        
+        moreDescOutlet.text = "\(appData.forecastList[appData.index].weather[0].description)"
     }
     
 
